5feed559b96dbeca434930ed618d6b16f2aeed61
Javier Alvarez Torrejon <jalvareztorrejon97@gmail.com>
2023-08-11 16:43:11 +0200

    Prueba Merge

--- WhatWeDo/Models/Evento.cs
+++ WhatWeDo/Models/Evento.cs
@@ -7,5 +7,6 @@ namespace WhatWeDo.Models
         public string ImgSource { get; set; }
         public string TituloEvento { get; set; }       
         public string DescEvento { get; set; }
+       
     }
 }
