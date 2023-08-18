3c97205f73cfd9575e08f83a519f642775c38e6e
jhonyUPC <jhonatan.covarrubias@estudiantat.upc.edu>
2023-08-18 19:09:44 +0200

    DAL y Model T027 T029 T030 T033

---
+++ WhatWeDo/Models/Ubicacion.cs
@@ -0,0 +1,19 @@
+﻿namespace WhatWeDo.Models
+{
+    public class Ubicacion
+    {
+        public int IdUbicacion { get; set; }
+        public string Direccion { get; set; }
+
+        public Ubicacion()
+        {
+
+        }
+
+        public Ubicacion(int idUbicacion, string direccion)
+        {
+            this.IdUbicacion = idUbicacion;
+            this.Direccion = direccion;
+        }
+    }
+}
---
+++ WhatWeDo/Servicios/Implementacion/CategoriaService.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Extensions.Options;
+using System.Data.SqlClient;
+using System.Data;
+using WhatWeDo.Models;
+using WhatWeDo.Servicios.Configuracion;
+using WhatWeDo.Servicios.Contratos;
+
+namespace WhatWeDo.Servicios.Implementacion
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly ConfiguracionConexion _conexion;
+
+        public CategoriaService(IOptions<ConfiguracionConexion> conexion)
+        {
+            _conexion = conexion.Value;
+        }
+
+        public async Task DeleteCategoria(Categoria oCategoria)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(_conexion.CadenaBBDD))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_DeleteCategoria", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@IdCategoria", oCategoria.IdCategoria));
+                    conexion.Open();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Categoria> GetCategoria(Categoria oCategoria)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(_conexion.CadenaBBDD))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_GetCategoriaPorId", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@IdCategoria", oCategoria.IdCategoria));
+
+                    conexion.Open();
+                    using (SqlDataReader dr = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await dr.ReadAsync())
+                        {
+                            oCategoria.IdCategoria = Convert.ToInt32(dr["IdCategoria"]);
+                            oCategoria.Nombre = dr["Nombre"].ToString();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+            return oCategoria;
+        }
+
+        public async Task InsertCategoria(Categoria oCategoria)
+        {
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(_conexion.CadenaBBDD))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_InsertCategoria", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Nombre", oCategoria.Nombre));
+                    
+                    conexion.Open();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task UpdateCategoria(Categoria oCategoria)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(_conexion.CadenaBBDD))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_UpdateCategoria", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@IdCategoria", oCategoria.IdCategoria));
+                    cmd.Parameters.Add(new SqlParameter("@Nombre", oCategoria.Nombre));
+
+                    conexion.Open();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
